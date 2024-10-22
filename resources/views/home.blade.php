039cf53a9ef66c2be32c3be83c6c1988e07a7407
Noah <48385324+NoahFsc@users.noreply.github.com>
2024-10-22 18:02:34 +0200

    Config Tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,34 +4,6 @@
 
 @section('contenu')
 
-<nav class="p-4 bg-gray-800">
-    <div class="container flex items-center justify-between mx-auto">
-        <div class="text-2xl text-white">VroumVroum 🚗💨</div>
-        <div class="flex gap-4">
-            <a href="{{ route('home') }}" class="text-white hover:text-gray-400">Accueil</a>
-            <a href="{{ route('home') }}" class="text-white hover:text-gray-400">À propos</a>
-            <a href="{{ route('home') }}" class="text-white hover:text-gray-400">Contact</a>
-        </div>
-        <div class="flex gap-4">
-            @auth
-                <span class="flex items-center">
-                    <span class="w-3 h-3 mr-2 bg-green-500 rounded-full"></span>
-                    <span class="text-white">{{ Auth::user()->name }}</span>
-                </span>
-                <form action="{{ route('auth.logout') }}" method="POST" class="inline">
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit" class="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none">Déconnexion</button>
-                </form>
-            @endauth
-
-            @guest
-                <a href="{{ route('auth.login') }}" class="px-4 py-2 text-white rounded-md bg-slate-600 hover:bg-slate-500 focus:outline-none">Connexion</a>
-            @endguest
-        </div>
-    </div>
-</nav>
-
 <div class="container mx-auto mt-8">
     @auth
         <div class="mt-4 text-2xl">Bienvenue <b>{{ Auth::user()->name }}</b> 👋,</div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,11 +11,38 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
     </head>
-    <body class="text-white bg-gray-800">
+    <body class="bg-background">
+        <nav class="p-8">
+            <div class="container flex items-center justify-between mx-auto">
+                <div class="text-2xl">VroumVroum 🚗💨</div>
+                <div class="flex items-center gap-12">
+                    <a href="{{ route('home') }}" class="nav-link {{ Route::is('home') ? 'active' : '' }}">Accueil</a>
+                    <a href="{{ route('home') }}" class="nav-link">Acheter</a>
+                    <a href="{{ route('home') }}" class="nav-link">Enchérir</a>
+                    <a href="{{ route('home') }}" class="nav-link">Vendre</a>
+                    @auth
+                        <span class="flex items-center">
+                            <span class="w-3 h-3 mr-2 bg-green-500 rounded-full"></span>
+                            <span>{{ Auth::user()->name }}</span>
+                        </span>
+                        <form action="{{ route('auth.logout') }}" method="POST" class="inline">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="px-4 py-2 text-white bg-error-500 rounded-md hover:bg-error-400 transition-all duration-300">Déconnexion</button>
+                        </form>
+                    @endauth
+        
+                    @guest
+                        <a class="px-4 py-2 text-white rounded-lg bg-primary-500 hover:bg-primary-400 transition-all duration-300" href="{{ route('auth.login') }}">Connexion</a>
+                    @endguest
+                </div>
+            </div>
+        </nav>
+
         <div class="mx-16 my-16">
 
             @yield('contenu')
-
+            
         </div>
     </body>
 </html>
