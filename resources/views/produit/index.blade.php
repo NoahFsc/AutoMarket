a3dbf83a39ae74de9ee73bf19edc86d9d38da958
Mathéo Stevance <matheo.stevance@etud.u-picardie.fr>
2024-12-02 13:13:13 +0100

    Information voiture

--- resources/views/produit/index.blade.php
+++ resources/views/produit/index.blade.php
@@ -21,33 +21,89 @@
         </div>
     </div>
 
-            <!-- Informations générales -->
-    <div class="w-5/6 mx-auto mt-4 bg-white">
-        <h2 class="text-lg font-medium">Informations générales</h2>
+    <!-- Informations générales -->
+    <div class="w-5/6 p-4 mx-auto mt-4 bg-white rounded-lg shadow-lg">
+        <div class="relative">
+            <div class="flex items-center space-x-2">
+                <i class="fa-light fa-circle-info"></i>
+                <h2 class="text-lg font-medium">Informations générales</h2>
+            </div>
+            <div class="absolute inset-x-0 bottom-0 h-0.5 bg-gray-300 opacity-50"></div>
+        </div>
+            <ul class="mt-2 space-y-2">
+                <li class="flex justify-between">
+                <span class="text-xs opacity-50">Année du véhicule</span>
+                <span class="text-xs">2020</span>
+            </li>
+            <li class="flex justify-between">
+                <span class="text-xs opacity-50">Kilométrage</span>
+                <span class="text-xs">15 265</span>
+            </li>
+            <li class="flex justify-between">
+                <span class="text-xs opacity-50">Type de boîte</span>
+                <span class="text-xs">Manuelle</span>
+            </li>
+            <li class="flex justify-between">
+                <span class="text-xs opacity-50">Nombre de portes</span>
+                <span class="text-xs">5</span>
+            </li>
+            <li class="flex justify-between">
+                <span class="text-xs opacity-50">Provenance</span>
+                <span class="text-xs">France</span>
+            </li>
+            <li class="flex justify-between">
+                <span class="text-xs opacity-50">Certificat technique</span>
+                <span class="text-xs">Révisé</span>
+            </li>
+        </ul>
+    </div>
+
+    <!-- Informations moteur -->
+    <div class="w-5/6 p-4 mx-auto mt-4 bg-white rounded-lg shadow-lg">
+        <div class="relative">
+            <div class="flex items-center space-x-2">
+                <i class="fa-regular fa-car"></i>
+                <h2 class="text-lg font-medium">Moteur</h2>
+            </div>
+            <div class="absolute inset-x-0 bottom-0 h-0.5 bg-gray-300 opacity-50"></div>
+        </div>
         <ul class="mt-2 space-y-2">
             <li class="flex justify-between">
-                <span>Année du véhicule</span>
-                <span>2020</span>
+                <span class="text-xs opacity-50">Énergie</span>
+                <span class="text-xs">Électrique</span>
             </li>
             <li class="flex justify-between">
-                <span>Kilométrage</span>
-                <span>15 265</span>
+                <span class="text-xs opacity-50">Puissance Fiscale</span>
+                <span class="text-xs">8 CV</span>
             </li>
             <li class="flex justify-between">
-                <span>Type de boîte</span>
-                <span>Manuelle</span>
+                <span class="text-xs opacity-50">Puissance DIN</span>
+                <span class="text-xs">Manuelle</span>
+            </li>
+        </ul>
+    </div>
+
+    <!-- Informations moteur -->
+    <div class="w-5/6 p-4 mx-auto mt-4 bg-white rounded-lg shadow-lg">
+        <div class="relative">
+            <div class="flex items-center space-x-2">
+                <i class="fa-regular fa-cloud"></i>
+                <h2 class="text-lg font-medium">Impact</h2>
+            </div>
+            <div class="absolute inset-x-0 bottom-0 h-0.5 bg-gray-300 opacity-50"></div>
+        </div>
+        <ul class="mt-2 space-y-2">
+            <li class="flex justify-between">
+                <span class="text-xs opacity-50">Consommation</span>
+                <span class="text-xs">17kWh/100km</span>
             </li>
             <li class="flex justify-between">
-                <span>Nombre de portes</span>
-                <span>5</span>
+                <span class="text-xs opacity-50">Crit'air</span>
+                <span class="text-xs">8 CV</span>
             </li>
             <li class="flex justify-between">
-                <span>Provenance</span>
-                <span>France</span>
-            </li>
-            <li class="flex justify-between">
-                <span>Certificat technique</span>
-                <span>Révisé</span>
+                <span class="text-xs opacity-50">Émission</span>
+                <span class="text-xs">5</span>
             </li>
         </ul>
     </div>
