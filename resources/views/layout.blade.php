5d88942b5388444a043cf2753d064d9f9a5e8c7f
Noah <48385324+NoahFsc@users.noreply.github.com>
2024-12-05 14:39:12 +0100

    Correction barre bleue navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -31,9 +31,9 @@
             </div>
             <div class="items-center hidden gap-12 md:flex">
                 <a href="{{ route('home') }}" class="nav-link {{ Route::is('home') ? 'active' : '' }}">Accueil</a>
-                <a href="{{ route('acheter.index') }}" class="nav-link">Acheter</a>
-                <a href="{{ route('home') }}" class="nav-link">Enchérir</a>
-                <a href="{{ route('home') }}" class="nav-link">Vendre</a>
+                <a href="{{ route('acheter.index') }}" class="nav-link {{ Route::is('acheter.index') ? 'active' : '' }}">Acheter</a>
+                <a href="{{ route('home') }}" class="nav-link {{ Route::is('home') ? 'active' : '' }}">Enchérir</a>
+                <a href="{{ route('home') }}" class="nav-link {{ Route::is('home') ? 'active' : '' }}">Vendre</a>
 
                 @auth
                 {{-- Composant WireUI Dropdown --}}
