976ca978785bb799442111f6c0aa08413083e064
Flymarvel <florent.hallot@gmail.com>
2024-12-05 15:54:38 +0100

    k

--- app/Http/Controllers/VendreController.php
+++ app/Http/Controllers/VendreController.php
@@ -7,13 +7,19 @@ use App\Models\WebsiteReview;
 use App\Models\Brand;
 use App\Models\CarModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class VendreController extends Controller
 {
     public function index()
     {
-        $sells = Car::where('vente_enchere', 0)->get();
-        $auctions = Car::where('vente_enchere', 1)->get();
+        $sells = Car::whereHas('user', function ($query) {
+            $query->where('id', Auth::id());
+        })->where('vente_enchere', 0)->get();
+
+        $auctions = Car::whereHas('user', function ($query) {
+            $query->where('id', Auth::id());
+        })->where('vente_enchere', 1)->get();
 
         return view('vendre.index', compact('sells', 'auctions'));
     }
--- resources/views/components/sell-menu.blade.php
+++ resources/views/components/sell-menu.blade.php
@@ -1,3 +1,25 @@
 <div>
-    <p>UWU</p>
+    <!-- Vos ventes en cours -->
+    <section class="py-8 mx-auto md:mt-32">
+        <div class="flex justify-between">
+            <h2 class="text-2xl font-bold mb-4">Vos ventes en cours</h2>
+        </div>
+        <div class="grid grid-cols-auto-fit-card gap-6">
+            @foreach ($sells as $car)
+            @livewire('selling-card', ['car' => $car])
+            @endforeach
+        </div>
+    </section>
+
+    <!-- Vos enchères en cours -->
+    <section class="py-8 mx-auto md:mt-16">
+        <div class="flex justify-between">
+            <h2 class="text-2xl font-bold mb-4">Vos enchères en cours</h2>
+        </div>
+        <div class="grid grid-cols-auto-fit-card gap-6">
+            @foreach ($auctions as $car)
+            @livewire('auction-card', ['car' => $car])
+            @endforeach
+        </div>
+    </section>
 </div>
