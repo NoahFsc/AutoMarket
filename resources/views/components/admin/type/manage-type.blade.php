cc97e44014508f509b8dc1ed0320da3c10269e03
Noah <48385324+NoahFsc@users.noreply.github.com>
2025-01-04 19:16:24 +0100

    Dernier fix apparition alpine

--- resources/views/components/admin/type/manage-type.blade.php
+++ resources/views/components/admin/type/manage-type.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ open: false, vehicle_type_id: @entangle('type_of_car_id') }" @open-manage-type-modal.window="open = true; vehicle_type_id = $event.detail.vehicle_type_id; $wire.openModal(vehicle_type_id)" @close-manage-type-modal.window="open = false">
     <button @click="open = true; $wire.openModal()" class="px-4 py-2 text-white rounded-md bg-primary-500 hover:bg-primary-600"><i class="fa-solid fa-plus"></i> Ajouter un type de véhicule</button>
 
-    <div x-show="open" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" @click="open = false">
+    <div x-cloak x-show="open" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" @click="open = false">
         <div class="w-full max-w-md p-6 bg-white rounded-lg shadow-lg" @click.stop>
             <div class="flex items-center gap-2 mb-4">
                 <i class="text-3xl fa-regular fa-plus"></i>
